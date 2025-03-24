84bb87be6b02df2cb20e84f890cfa2b782698bbc
VathareVinayak <vinayak.vathare2004@gmail.com>
2025-03-24 15:22:25 +0530

    Production setup

--- doeproject/doeproject/settings.py
+++ doeproject/doeproject/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = 'django-insecure-k1xrn8cki9w5(ea0e4t2c-rn$=_$j-z%nsl+9n6ne!f48kz(3h'
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
